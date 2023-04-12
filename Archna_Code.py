3bca28cefa9b4c37d7301d9da5d35b2b90130c65
Archna-12 <130336319+Archna-12@users.noreply.github.com>
2023-04-12 21:55:44 +0530

    Update Archna_Code.py

--- Archna_Code.py
+++ Archna_Code.py
@@ -1,78 +1,51 @@
+#Importing Required Libraries
+from bs4 import BeautifulSoup
 import requests
 import csv
-import sqlite3
 import datetime
-from bs4 import BeautifulSoup
+import sqlite3
+import pandas as pd
 
+#Defining Page for Scraping
+page = requests.get("https://theverge.com")
+soup = BeautifulSoup(page.content, "html.parser")
 
-class Article:
-    def __init__(self, headline, url, author, date):
-        self.headline = headline
-        self.url = url
-        self.author = author
-        self.date = date
+#Getting Date to use as CSV filename
+date = datetime.datetime.now()
+file = date.strftime('%d%m%Y_verge.csv')
 
+#Header for our CSV file
+header = ['ID', 'URL', 'Headline', 'Author', 'Date/Time']
+with open(file, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
 
-class VergeScraper:
-    def __init__(self):
-        self.articles = []
-        self.base_url = 'https://www.theverge.com'
+#Getting All Articles
+all_link = soup.find_all("a", {"data-chorus-optimize-field": "hed"})
 
-    def scrape(self):
-        # Send a GET request to theverge.com and parse the HTML
-        response = requests.get(self.base_url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+id=1
+#Loop for Getting Each Article, Extracting URL, Author Name and Date and then Writing the data into CSV file.
+for link in all_link:
+    name = []
+    try:
+        ind_page = requests.get(link.get('href'))
+    except requests.exceptions.ReadTimeout:
+        print("Request Timeout")
+    ind_soup = BeautifulSoup(ind_page.content, "html.parser")
+    time = ind_soup.find("time",{"class": "c-byline__item"})
+    author_name = ind_soup.find_all("span",{"class": "c-byline__author-name"})
+    for text in author_name:
+        name.append(text.text)
+    author = ', '.join(name)
+    data = [id,link.get('href'),link.text.strip(),author,time.text.strip()]
+    with open(file, 'a', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+    id = id+1
 
-        # Find all article elements
-        article_elements = soup.find_all('article')
-
-        # Extract article data and create Article objects
-        for article_element in article_elements:
-            headline = article_element.find('h2').text.strip()
-            url = article_element.find('a')['href']
-            author = article_element.find('span', class_='c-byline__author').text.strip()
-            date = article_element.find('time')['datetime']
-            article = Article(headline, url, author, date)
-            self.articles.append(article)
-
-    def save_to_csv(self):
-        # Generate the CSV filename based on the current date
-        today = datetime.datetime.now().strftime('%d%m%Y')
-        filename = f'{today}_verge.csv'
-
-        # Write article data to the CSV file
-        with open(filename, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['id', 'URL', 'headline', 'author', 'date'])
-            for i, article in enumerate(self.articles, start=1):
-                writer.writerow([i, article.url, article.headline, article.author, article.date])
-
-        print(f'Saved articles to {filename}')
-
-    def save_to_sqlite(self):
-        # Generate the SQLite database filename based on the current date
-        today = datetime.datetime.now().strftime('%d%m%Y')
-        filename = f'{today}_verge.db'
-
-        # Create SQLite database and table
-        conn = sqlite3.connect(filename)
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS articles
-                     (id INTEGER PRIMARY KEY, URL TEXT, headline TEXT, author TEXT, date TEXT)''')
-
-        # Insert article data into the table
-        for i, article in enumerate(self.articles, start=1):
-            c.execute("INSERT INTO articles (id, URL, headline, author, date) VALUES (?, ?, ?, ?, ?)",
-                      (i, article.url, article.headline, article.author, article.date))
-
-        conn.commit()
-        conn.close()
-
-        print(f'Saved articles to {filename}')
-
-
-if __name__ == '__main__':
-    scraper = VergeScraper()
-    scraper.scrape()
-    scraper.save_to_csv()
+#Saving the data in SQLite Database
+conn = sqlite3.connect('theverge.db')
+stud_data = pd.read_csv(file)
+stud_data.to_sql(file, conn, if_exists='replace', index=False)
+conn.close()
     scraper.save_to_sqlite()
